d611fc31a7720a5eef14cbca70e4b1e12a2e1fb9
EMMA JOHNSON-BARTH <emma.johnson9013@gmail.com>
2023-09-18 14:19:20 -0500

    got the sales data kinda working a little

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ const seattleShop = {
     minCustHr: 23,
     maxCustHr: 65,
     avgCookiePerSale: 6.3,
-    randomCustPerHour: function(){
+    randomCustPerHour: function () {
         return Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1) + this.minCustHr);
     },
     cookiesPerHourArr: [],
@@ -16,7 +16,7 @@ const tokyoShop = {
     minCustHr: 3,
     maxCustHr: 24,
     avgCookiePerSale: 1.2,
-    randomCustPerHour: function(){
+    randomCustPerHour: function () {
         return Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1) + this.minCustHr);
     },
     cookiesPerHourArr: [],
@@ -26,7 +26,7 @@ const dubaiShop = {
     minCustHr: 11,
     maxCustHr: 38,
     avgCookiePerSale: 3.7,
-    randomCustPerHour: function(){
+    randomCustPerHour: function () {
         return Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1) + this.minCustHr);
     },
     cookiesPerHourArr: [],
@@ -36,7 +36,7 @@ const parisShop = {
     minCustHr: 20,
     maxCustHr: 38,
     avgCookiePerSale: 2.3,
-    randomCustPerHour: function(){
+    randomCustPerHour: function () {
         return Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1) + this.minCustHr);
     },
     cookiesPerHourArr: [],
@@ -46,7 +46,7 @@ const limaShop = {
     minCustHr: 2,
     maxCustHr: 16,
     avgCookiePerSale: 4.6,
-    randomCustPerHour: function(){
+    randomCustPerHour: function () {
         return Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1) + this.minCustHr);
     },
     cookiesPerHourArr: [],
@@ -55,23 +55,34 @@ const limaShop = {
 
 
 
-function populateHourlyUl() {
 
-// Create an "li" node:
-const node = document.createElement("li");
 
-// Create a text node:
-const textnode = document.createTextNode(randomCustPerHour());
 
-// Append the text node to the "li" node:
-node.appendChild(textnode);
 
-// Append the "li" node to the list:
-document.getElementById("seattle").appendChild(node);
+function populateArray(shop, hoursOpen) {
+    for (let i = 0; i < hoursOpen - 1; i++) {
+        shop.cookiesPerHourArr.push(shop.randomCustPerHour())
+    }
 }
 
+populateArray(seattleShop, 14);
 
-// seattleShop.cookiesPerHourArr.push(randomCustPerHour());
+console.log(seattleShop.cookiesPerHourArr)
+
+function populateHourlyUl(shop, city) {
+    // Create an "li" node:
+    const node = document.createElement("li");
+    // Create a text node:
+    const textnode = document.createTextNode(shop.randomCustPerHour());
+    // Append the text node to the "li" node:
+    node.appendChild(textnode);
+    // Append the "li" node to the list:
+    document.getElementById("city").appendChild(node);
+}
+ populateHourlyUl(seattleShop);
+
+
+
 // let petUl = document.createElement('ul');
 // article.appendChild(petUl);
 // //                object.array go through the whole array
