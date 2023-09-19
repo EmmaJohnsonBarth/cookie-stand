81f6cb90d3a040743382de0c7a79372f06ad74ce
tektechnologies <info@tektechnologies.com>
2023-09-19 09:43:13 -0500

    format js and html add title

--- js/app.js
+++ js/app.js
@@ -1,77 +1,84 @@
-'use strict'
+'use strict';
 
-console.log('js is connected.')
+console.log('js is connected.');
 
 const seattleShop = {
-    cityName: "Seattle",
-    minCustHr: 23,
-    maxCustHr: 65,
-    avgCookiePerSale: 6.3,
-    hoursOpen: 14,
-    cookiesSold: 0,
-    randomCustPerHour: function () {
-        return Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1) + this.minCustHr);
-    },
-    cookiesPerHourArr: [],
+  //property attribute... key: value pair.......
+  cityName: 'Seattle',
+  //our keys' data type is a string... bracket notation
+  minCustHr: 23,
+  maxCustHr: 65,
+  //key           : value
+  avgCookiePerSale: 6.3,
+  hoursOpen: 14,
+  cookiesSold: 0,
+  //name/key            anonymous function....
+  randomCustPerHour: function () {
+    //contextual this     it refers to an instance of an object
+    return Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1) + this.minCustHr);
+  },
+  cookiesPerHourArr: [],
 };
 
 const tokyoShop = {
-    cityName: "Tokyo",
-    minCustHr: 3,
-    maxCustHr: 24,
-    avgCookiePerSale: 1.2,
-    hoursOpen: 14,
-    cookiesSold: 0,
-    randomCustPerHour: function () {
-        return Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1) + this.minCustHr);
-    },
-    cookiesPerHourArr: [],
+  cityName: 'Tokyo',
+  minCustHr: 3,
+  maxCustHr: 24,
+  avgCookiePerSale: 1.2,
+  hoursOpen: 14,
+  cookiesSold: 0,
+  randomCustPerHour: function () {
+    return Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1) + this.minCustHr);
+  },
+  cookiesPerHourArr: [],
 };
 
 const dubaiShop = {
-    cityName: "Dubai",
-    minCustHr: 11,
-    maxCustHr: 38,
-    avgCookiePerSale: 3.7,
-    hoursOpen: 14,
-    cookiesSold: 0,
-    randomCustPerHour: function () {
-        return Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1) + this.minCustHr);
-    },
-    cookiesPerHourArr: [],
+  cityName: 'Dubai',
+  minCustHr: 11,
+  maxCustHr: 38,
+  avgCookiePerSale: 3.7,
+  hoursOpen: 14,
+  cookiesSold: 0,
+  randomCustPerHour: function () {
+    return Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1) + this.minCustHr);
+  },
+  cookiesPerHourArr: [],
 };
 
 const parisShop = {
-    cityName: "Paris",
-    minCustHr: 20,
-    maxCustHr: 38,
-    avgCookiePerSale: 2.3,
-    hoursOpen: 14,
-    cookiesSold: 0,
-    randomCustPerHour: function () {
-        return Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1) + this.minCustHr);
-    },
-    cookiesPerHourArr: [],
+  cityName: 'Paris',
+  minCustHr: 20,
+  maxCustHr: 38,
+  avgCookiePerSale: 2.3,
+  hoursOpen: 14,
+  cookiesSold: 0,
+  randomCustPerHour: function () {
+    return Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1) + this.minCustHr);
+  },
+  cookiesPerHourArr: [],
 };
 
 const limaShop = {
-    cityName: "Lima",
-    minCustHr: 2,
-    maxCustHr: 16,
-    avgCookiePerSale: 4.6,
-    hoursOpen: 14,
-    cookiesSold: 0,
-    randomCustPerHour: function () {
-        return Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1) + this.minCustHr);
-    },
-    cookiesPerHourArr: [],
+  cityName: 'Lima',
+  minCustHr: 2,
+  maxCustHr: 16,
+  avgCookiePerSale: 4.6,
+  hoursOpen: 14,
+  cookiesSold: 0,
+  randomCustPerHour: function () {
+    return Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1) + this.minCustHr);
+  },
+  cookiesPerHourArr: [],
 };
 
 function populateArray(shop) {
-    for (let i = 0; i < 13; i++) {
-        shop.cookiesPerHourArr.push(shop.randomCustPerHour())
-        // shop.cookiesSold += shop.cookiesPerHour[i]
-    }
+  console.log('shop objects:', shop);
+  for (let i = 0; i < 13; i++) {
+
+    shop.cookiesPerHourArr.push(shop.randomCustPerHour());
+    // shop.cookiesSold += shop.cookiesPerHour[i]
+  }
 }
 
 populateArray(seattleShop);
@@ -80,11 +87,11 @@ populateArray(dubaiShop);
 populateArray(parisShop);
 populateArray(limaShop);
 
-console.log("seattle cookies per hour: " + seattleShop.cookiesPerHourArr)
-console.log("tokyp cookies per hour: " + tokyoShop.cookiesPerHourArr)
-console.log("dubai cookies per hour: " + dubaiShop.cookiesPerHourArr)
-console.log("paris cookies per hour: " + parisShop.cookiesPerHourArr)
-console.log("lima cookies per hour: " + limaShop.cookiesPerHourArr)
+// console.log('seattle cookies per hour: ' + seattleShop.cookiesPerHourArr);
+// console.log('tokyp cookies per hour: ' + tokyoShop.cookiesPerHourArr);
+// console.log('dubai cookies per hour: ' + dubaiShop.cookiesPerHourArr);
+// console.log('paris cookies per hour: ' + parisShop.cookiesPerHourArr);
+// console.log('lima cookies per hour: ' + limaShop.cookiesPerHourArr);
 
 // function populateHourlyUl(shop) {
 //     const node = document.createElement("li");
@@ -107,19 +114,19 @@ console.log("lima cookies per hour: " + limaShop.cookiesPerHourArr)
 // }
 
 function populateHourlyUl(shop) {
-    // document.title(shop);
-    const header = document.querySelector('h1');
-    header.textContent = shop.cityName;
+  // document.title(shop);
+  const header = document.querySelector('h1');
+  header.textContent = shop.cityName;
 
-    const cityList = document.getElementById("city");
-    for (let i = 0; i < shop.hoursOpen -1; i++) {
-        const node = document.createElement('li');
-        node.textContent = `${i + 6}:00 ${shop.cookiesPerHourArr[i]} cookies`;
-        cityList.appendChild(node);
-    }
-    const totalNode = document.createElement('li');
-    // totalNode.textContent(`Total: ${shop.cookiesSold} cookies`);
-    cityList.appendChild(totalNode)
+  const cityList = document.getElementById('city');
+  for (let i = 0; i < shop.hoursOpen -1; i++) {
+    const node = document.createElement('li');
+    node.textContent = `${i + 6}:00 ${shop.cookiesPerHourArr[i]} cookies`;
+    cityList.appendChild(node);
+  }
+  const totalNode = document.createElement('li');
+  // totalNode.textContent(`Total: ${shop.cookiesSold} cookies`);
+  cityList.appendChild(totalNode);
 }
 
 populateHourlyUl(seattleShop);
@@ -134,6 +141,6 @@ populateHourlyUl(parisShop);
 //   petUl.appendChild(petLi);
 // }
 
-//petUl -> 
+//petUl ->
 //petOne -> seattleShop
 //petLi -> customersperhour
