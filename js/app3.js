2b0308c8fb8d660b966a879b8440f43247f15bd8
EMMA JOHNSON-BARTH <emma.johnson9013@gmail.com>
2023-09-19 01:00:58 -0500

    tried adding addEventListener but that didnt work

--- js/app3.js
+++ js/app3.js
@@ -1,3 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+console.log('app3.js is connected')
+
+
 const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
 function Shop(cityName, minCustHr, maxCustHr, avgCookiesPerSale) {
@@ -101,3 +106,5 @@ thead.appendChild(headerRow);
 
 const footerRow = generateFooterRow();
 tbody.appendChild(footerRow);
+
+});
